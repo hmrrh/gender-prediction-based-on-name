00762ee15a5bd2690fe452707e3de82c6c01cbf1
hmrrh <hmrmdhn.rh@gmail.com>
2024-02-25 23:19:03 +0700

    initial app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Gender Prediction Based on Name')
+st.write("'Give it a shot! Let us know, who's your name?'")
+
+user_input = st.text_input('Enter a name: ')
+st.write('Your gender is: ', )
+st.write('Accuracy: ', )
